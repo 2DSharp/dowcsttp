ef18b13894bc8516cd2c87d2836341ba029d0ef9
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-12-22 12:11:43 +0530

    Update (Too lazy to find out what the commit is)

--- src/main/java/me/twodee/dowcsttp/service/Accounts.java
+++ src/main/java/me/twodee/dowcsttp/service/Accounts.java
@@ -10,23 +10,24 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import javax.servlet.http.HttpSession;
 
 @Service
 public class Accounts {
 
     private final UserIdentityRepository repository;
+    private final HttpSession session;
 
-    public Accounts(UserIdentityRepository repository) {
+    public Accounts(UserIdentityRepository repository, HttpSession session, HttpSession session1) {
         this.repository = repository;
+        this.session = session1;
     }
 
-////    public boolean hasCorrectCredentials(Authorization.LoginIdentity loginIdentity) {
-////
-////        var storedIdentity = repository.findById(loginIdentity.identifier);
-////        return (storedIdentity.isPresent() && passwordIsCorrect(loginIdentity.password, storedIdentity.get().password));
-////    }
+    public boolean hasCorrectCredentials(User.LoginData loginIdentity) {
+
+        var storedIdentity = repository.findByEmail(loginIdentity.identifier);
+        return (storedIdentity.isPresent() && passwordIsCorrect(loginIdentity.password, storedIdentity.get().getHashedPassword()));
+    }
 
     private boolean passwordIsCorrect(String plaintext, String hashed) {
         return BCrypt.checkpw(plaintext, hashed);
@@ -35,7 +36,10 @@ public class Accounts {
     public ResultObject register(User.RegistrationData data) {
         try {
             if (repository.existsUserIdentityByEmail(data.email)) {
-                return ResultObject.builder().isSuccessful(false).error("The email you provided is already in use").build();
+                return ResultObject.builder()
+                        .isSuccessful(false)
+                        .error("The email you provided is already in use")
+                        .build();
             }
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             String hashedPassword = encoder.encode(data.password);
@@ -47,7 +51,16 @@ public class Accounts {
 
             return ResultObject.builder().isSuccessful(true).build();
         } catch (DataAccessException e) {
-            return ResultObject.builder().isSuccessful(false).error("Something went wrong, try again").build();
+            return ResultObject.builder()
+                    .isSuccessful(false)
+                    .error("Something went wrong, try again")
+                    .build();
         }
+    }
+
+    public void login(String identifier) {
+        UserIdentity identity = repository.findByEmail(identifier).get();
+        session.setAttribute("loggedIn", true);
+        session.setAttribute("user", identity);
     }
 }
